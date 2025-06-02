52732ecf70f0b623f40b75ebd54fd4c0cf97b725
liverHawk <hawkyofkn5365@gmail.com>
2025-06-02 15:44:03 +0900

    """ measureアプリケーションを新規作成し、関連するモデル、ビュー、テンプレート、URL設定を追加しました。また、uv.lockファイルからrpi-gpioの依存関係を削除しました。 """

---
+++ measure/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .measure import get_temp_hum
+
+def index(request):
+    return render(request, 'measure/index.html')
+
+def measure(request):
+    time, temperature, humidity = get_temp_hum()
+    context = {
+        'time': time,
+        'temperature': temperature,
+        'humidity': humidity,
+    }
+    return render(request, 'measure/measure.html', context)
